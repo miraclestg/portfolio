e8fb554e617433a4fa6dd8280be0fdbde0530f19
Ân Lý <lyhongphuongan@gmail.com>
2026-06-21 02:54:23 +0700

    Fix base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/", // For https://lyhongphuongan.github.io (user pages)
+  base: '/portfolio/', // For https://lyhongphuongan.github.io (user pages)
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
